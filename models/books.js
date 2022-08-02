6de37afe226c5111dd4c580eeee3b959f5c50bac
riansyah <mriansyah93@gmail.com>
2022-08-02 14:27:23 +0700

    create dummy data in json format

---
+++ models/books.js
@@ -0,0 +1,11 @@
+const books = [
+    {
+        "title":"Harry Potter 1",
+        "author": "JK Rowling"
+    },
+    {
+        "title":"Harry Potter 2",
+        "author": "JK Rowling"
+    }
+]
+module.exports = books
